f81fdab68a528bf1d9756d2f247d0db2f153d61f
DeniOS777 <Denios777@gmail.com>
2022-07-28 22:41:36 +0300

    change files project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = key => {
+    this.setState({ [key]: this.state[key] + 1 });
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div>
+        <h1>Please leave your feedback please</h1>
+        {Object.keys(this.state).map(key => (
+          <button onClick={() => this.handleClick(key)} key={key} type="button">
+            {key}
+          </button>
+        ))}
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
